0f866a2d821b79823d610542879f79fffccfc306
Biswajit Aich <0000biswajitaich@gmail.com>
2025-05-04 19:40:31 +0530

    compile err

--- app/api/check-bot-health/route.ts
+++ app/api/check-bot-health/route.ts
@@ -1,4 +1,4 @@
-// export async function GET() {
+export async function GET() {
 //     const API = process.env.NODE_ENV_BOT_API;
 //     try {
 //         if (!API) return new Response(JSON.stringify({ health: "notOk" }));
@@ -9,4 +9,4 @@
 //         console.error((error as Error).message);
 //         return new Response(JSON.stringify({ health: "notOk" }));
 //     }
-// }
+}
